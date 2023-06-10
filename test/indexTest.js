b86a2181f8791a260557736e2c6e2aa054810190
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-06-10 04:33:34 -0400

    work done

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,7 @@
 require ( './root.js' );
-
+function introduction(Aki) {
+  return `Hi, my name is ${Aki}.`;
+}
 
 describe('introduction(name)', function() {
   it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
@@ -7,14 +9,18 @@ describe('introduction(name)', function() {
     expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
   })
 })
-
+function introductionWithLanguage(Aki, JavaScript) {
+  return `Hi, my name is ${Aki} and I am learning to program in ${JavaScript}.`;
+}
 describe('introductionWithLanguage(name, language)', function() {
   it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
     expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
     expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
   })
 })
-
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and language defaults to JavaScript', function() {
     expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
